8f3e392bebda0d7a353ec63cf8c70cfc4f963a13
michaeltshen <mts247@cornell.edu>
2025-04-21 20:49:36 -0400

    Added fix to eval script for right batch size

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ python modeling/latency_sim.py \
     --latency-data data/profiling/hermes_cluster_latency.csv \
     --query-trace data/modeling/cluster_trace.csv \
     --retrieved-docs 5 \
-    --batch-size 1 \
+    --batch-size 32 \
     --sample-nprobe 8 \
     --deep-nprobe 128 \
     --num-threads 32 
@@ -19,7 +19,7 @@ python modeling/dvfs_sim.py \
     --query-trace data/modeling/cluster_trace.csv \
     --inference-trace data/profiling/inference_latency.csv \
     --retrieved-docs 5 \
-    --batch-size 1 \
+    --batch-size 32 \
     --sample-nprobe 8 \
     --deep-nprobe 128 \
     --num-threads 32 \
